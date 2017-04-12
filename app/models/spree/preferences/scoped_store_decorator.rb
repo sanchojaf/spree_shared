a57e09acc93d71d47548b5ab64806c4f7bbc8633
Miguel Sancho <sanchocuba@gmail.com>
2017-04-11 22:09:25 -0700

    change current_tenant by current

--- app/models/spree/preferences/scoped_store_decorator.rb
+++ app/models/spree/preferences/scoped_store_decorator.rb
@@ -3,6 +3,6 @@ Spree::Preferences::ScopedStore.class_eval do
     # hack hack hack ..
     # aka ENV['RAILS_CACHE_ID'] on a Spree default install
     def rails_cache_id
-      Apartment::Tenant.current_tenant
+      Apartment::Tenant.current
     end
 end
